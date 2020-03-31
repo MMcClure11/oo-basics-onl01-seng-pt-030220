f59bfcf22ffc9c6d5b82052cc8b7230501a187d0
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:41:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,3 +14,42 @@
 #   end 
   
 # end 
+
+
+class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
